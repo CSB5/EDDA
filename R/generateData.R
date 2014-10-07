380119e4a23091e3a0196ed053a4f38007c08821
Bioconductor Git-SVN Bridge <bioc-sync@bioconductor.org>
2014-10-07 02:15:21 -0700

    Commit made by the Bioconductor Git-SVN bridge.
    Consists of 0 commit.
    
    Commit information:

--- R/generateData.R
+++ R/generateData.R
@@ -2,7 +2,7 @@ generateData <- function(SimulModel = "Full", SampleVar = "medium",
     ControlRep = 5, CaseRep = ControlRep, EntityCount = 1000, 
     FC = "Norm(2,1)", perDiffAbund = 0.1, upPDA = perDiffAbund/2, 
     downPDA = perDiffAbund/2, numDataPoints = 100, AbundProfile = "HBR", modelFile = NULL, 
-    minAbund = 10, varLibsizes = 0.1, outlier=FALSE,perOutlier=0.15, factorOutlier=100, inputCount = NULL, inputLabel = NULL, 
+    minAbund = 10, varLibsizes = 0.1, inputCount = NULL, inputLabel = NULL, 
     SimulType = "auto") {
     
     #Check setting correct first to avoid meaningless waiting if wrong
@@ -25,11 +25,6 @@ generateData <- function(SimulModel = "Full", SampleVar = "medium",
       }else if (is.null(modelFile) && (is.null(inputCount) || is.null(inputLabel))){
         stop("ERROR: using ModelFree approach without model file and pilot data for sampling.")
       }
-    }
-    
-    if (outlier == TRUE && (perOutlier>1 || perOutlier<0))
-    {
-      stop("ERROR: Outlier parameter should be in range 0 to 1!")
     }
     
     model = SimulModel
@@ -676,22 +671,6 @@ generateData <- function(SimulModel = "Full", SampleVar = "medium",
             model.combined[, i] <- rmultinom(n = 1, size = libsizes[i], 
                 prob = model.freq[, i])
         }
-    }
-    
-    
-    if (outlier==TRUE && SimulModel != "ModelFree" && SimulModel != "ModelFreeMn"){
-      numG=dim(model.combined)[1]
-      numR=dim(model.combined)[2]
-      ind <- sample(1:numG, round(numG*perOutlier))
-      for (i in ind){
-        j <- sample(numR,1)
-        if (runif(1)>0.5) {
-          model.combined[i,j] <- round(model.combined[i,j]*factorOutlier)
-        }else{
-          model.combined[i,j] <- round(model.combined[i,j]/factorOutlier)
-        }
-        
-      }
     }
     return(list(count = model.combined, DiffAbundList = EDgenelist, 
         dataLabel = dataLabel))
